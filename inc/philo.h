b4df71eafdbbbaf8545ab479401c29b1823941cc
Basile REGNEAU <bregneau@student.42.fr>
2022-06-12 21:53:18 +0200

    WIP

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 17:53:58 by bregneau          #+#    #+#             */
-/*   Updated: 2022/06/11 19:23:04 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/06/12 21:52:24 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,16 @@ typedef struct s_data
 
 void	*ft_philo_routine(void *arg);
 
-
 //utils
 
 int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+size_t	ft_strlen(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+
+
 int		ft_return_error(char *message);
 int		ft_get_time(int d);
+void	ft_putstr_mutex(char *str, pthread_mutex_t *mutex);
 
 #endif
